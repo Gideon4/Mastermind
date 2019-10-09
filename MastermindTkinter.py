d752bf980b86961d64d951b6d37fd6b83f276e2f
Gideon4 <43590645+Gideon4@users.noreply.github.com>
2019-10-08 19:31:23 -0500

    Add files via upload

--- MastermindTkinter.py
+++ MastermindTkinter.py
@@ -10,41 +10,28 @@ white = 0
 black = 0
 count = 0
 currentguess = [""] * 4
+history = []
 
 def makesolution():
     global solution
     while len(solution) < 4:
         r = random.randint(0,5)
-        if r not in solution:
-            solution.append(r)
+        if colors[r] not in solution:
+            solution.append(colors[r])
 
 def score(guess):
-    global solution
-    global white
-    global black
+    global solution, white, black
     white = 0
     black = 0
     for i in range(4):
-        if guess[i] == str(solution[i]):
+        if guess[i] == solution[i]:
             white += 1
         else:
             for j in range(4):
-                if guess[i] == str(solution[j]):
+                if guess[i] == solution[j]:
                     black += 1
-    if white == 4:
-        print("You win. You got it in " + str(count) + " guesses.")
-    elif count == 8:
-        print("No one loves you and you suck at your only job.")
-    else:
-        print("Whites:" + str(white) + " & Blacks:" + str(black))
-        playerguess()
-
-def playerguess():
-    global count
-    print("Make a guess.")
-    guess = input()
-    count += 1
-    score(guess)
+    history[-1].append(white)
+    history[-1].append(black)
 
 def colorpicker(position, button):
     global colorwindow
@@ -68,12 +55,40 @@ def closecolorwindow(position, button, color):
     button.config(bg=color)
     currentguess[position] = color
 
+def submit():
+    global count
+    if "" not in currentguess:
+        history.append(currentguess.copy())
+        count += 1
+        score(currentguess)
+        draw()
+
 def draw():
     global canvas
     canvas.delete(tkinter.ALL)
-    canvas.create_rectangle(0, 0, cwidth, cheight, fill = "wheat")
+    canvas.create_rectangle(0, 0, cwidth, cheight, fill = "#ffffff")
+    xpos = 10
+    ypos = 10
+    for guess in history:
+        counter = 0
+        for color in guess:
+            if counter < 4: 
+                canvas.create_oval(xpos, ypos, xpos + 30, ypos + 30, fill = color)
+                xpos += 40
+            elif counter == 4:
+                for i in range(color):
+                    canvas.create_oval(xpos, ypos, xpos + 20, ypos + 20, fill = "#ffffff")
+                    xpos += 25
+            elif counter == 5:
+                for j in range(color):
+                    canvas.create_oval(xpos, ypos, xpos + 19, ypos + 19, fill = "#000000")
+                    xpos += 25
+            counter += 1
+        xpos = 10
+        ypos += 45
+        counter = 0
 
-root.config(bg="wheat")
+root.config(bg="#ffffff")
 
 b1 = tkinter.Button(root, width = 4, height = 2, bg = "#999999", command = lambda: colorpicker(0, b1))
 b1.grid(row = 1, column = 0)
@@ -87,11 +102,11 @@ b3.grid(row = 1, column = 2)
 b4 = tkinter.Button(root, width = 4, height = 2, bg = "#999999", command = lambda: colorpicker(3, b4))
 b4.grid(row = 1, column = 3)
 
-submit = tkinter.Button(root, text = "Submit Guess", padx = 10)
+submit = tkinter.Button(root, text = "Submit Guess", padx = 10, bg  = "#ff0000", fg = "#ffffff", font = "Arial 16 bold", command = submit)
 submit.grid(row = 1, column = 4, padx = 10)
 
-cwidth = 300
-cheight = 400
+cwidth = 350
+cheight =400
 
 canvas = tkinter.Canvas(root, width = cwidth, height = cheight)
 canvas.grid(row = 2, column = 0, columnspan = 5)
